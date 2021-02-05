7bc68d96ad86c7e03aca8980c05c2935bccbeb21
Amitrajit31 <amitrajitdas31@gmail.com>
2021-02-05 19:24:57 +0530

    renaming app.js as index.js to avoid confusion

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import mongoose from 'mongoose'
+/*import morgan from 'morgan'
+import userRouter from './routes/UserRoutes'
+import homeRouter from './routes/HomeRoutes'*/
+import dotenv from 'dotenv'
+
+// express
+const app = express();
+app.use(express.json());
+
+//  Loads .env file contents into | process.env
+dotenv.config();
+
+
+//middleware
+
+//app.use(morgan());//
+
+// routes
+
+/*app.use('/home', homeRouter);
+app.use('/user', userRouter);*/
+
+// server port 
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+);
+ 
+
+// mongodb connection
+
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+    if (err) {
+        console.log("connection error:", err);
+    } else {
+        console.log("MongoDB connection successful");
+    }
+});
+
+
