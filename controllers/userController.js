7d209e933ab86f83df50266c863be36aa51a80da
Amitrajit31 <amitrajitdas31@gmail.com>
2021-02-22 12:27:46 +0530

    user signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,26 @@
 import { User } from '../models/user.js';
 
+
+
+export const getUser = async (req, res) => {
+
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (err) {
+        res.status(400).send(err)
+    }
+};
+
+
 export const postUser = async (req, res) => {
 
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         res.send(user)
-    } catch (e) {
-        res.status(400).send()
+    } catch (err) {
+        res.status(400).send(err)
     }
 };
