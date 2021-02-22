fc2e93cb0355dcb635638f2e8ccd08575df85290
Amitrajit31 <amitrajitdas31@gmail.com>
2021-02-22 16:43:07 +0530

    signup converted to post req

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,7 @@ import { userSignup, userLogin } from '../controllers/userController.js';
 // express router
 const router = express.Router();
 
-router.get('/user/signup', userSignup);
+router.post('/user/signup', userSignup);
 router.post('/user/login', userLogin);
 
  export default router;
